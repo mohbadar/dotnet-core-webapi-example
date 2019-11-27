44caf006a8e5d748739e90fdcd496ab1c23cf8d1
mohbadar <mohammadbadarhashimi@gmail.com>
2019-11-27 15:11:10 +0430

    api webapp

---
+++ Contexts/TodoContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Contexts {
+    public class TodoContext: DbContext {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems {get; set;}
+    }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Models {
+    public class TodoItem {
+        public int Id {get; set;}
+        public string name{get; set;}
+        public bool isCompleted {get; set;}
+    }
+}
+
